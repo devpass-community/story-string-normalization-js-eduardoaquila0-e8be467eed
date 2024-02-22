4d1f77f13397f5db81be5f23b6cf819538cdd703
EduardoAquila <eduardoeduvivo@gmail.com>
2024-02-22 12:17:35 -0400

    Alteração string normalize 02

--- removeAccent.js
+++ removeAccent.js
@@ -1,16 +1,6 @@
 
 function removeAccent(str) {
-    return str.normalize('NFD').replace(/[\u0300-\u36f]/g, "")
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
 }
 
 module.exports = removeAccent;
-
-
-/* 
-var frase = "á ã ê ò";
-
-var semAcento = frase.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-
-
-console.log(semAcento);
-*/
